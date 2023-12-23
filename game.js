af3686e0666ef28786e5341cf8ef067b6c81d123
Ben Lim <finaldessert@gmail.com>
2023-12-22 21:50:15 -0500

    chore: initialize images & sounds in game.js

--- game.js
+++ game.js
@@ -1,2 +1,21 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+
+let frames = 0;
+const DEGREE = Math.PI/180;
+
+const sprite = new Image();
+sprite.src = 'images/sprite.png';
+
+const SCORE = new Audio();
+const FLAP = new Audio();
+const HIT = new Audio();
+const SWOOSHING = new Audio();
+const DIE = new Audio();
+
+SCORE.src = 'sounds/sfx_point.wav';
+FLAP.src = 'sounds/sfx_flap.wav';
+HIT.src = 'sounds/sfx_hit.wav';
+SWOOSHING.src = 'sounds/sfx_swooshing.wav';
+DIE.src = 'sounds/sfx_die.wav';
+
